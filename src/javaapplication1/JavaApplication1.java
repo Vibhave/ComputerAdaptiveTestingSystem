23fc08e2882312a67b2068199745ae484b737fa0
Vibhave <vibhav.bhave@gmail.com>
2014-08-26 11:21:58 +0530

    Added Local Files

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,62 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+/**
+ *
+ * @author jyoti
+ */
+public class JavaApplication1 {
+
+    public JavaApplication1() {
+        //
+    }
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        /* Set the Nimbus look and feel */
+        try {
+			UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
+		}//<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(NewJFrame1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(NewJFrame1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(NewJFrame1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(NewJFrame1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        
+        
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+          new NewJFrame1().setVisible(true);
+               
+            }
+        
+        // TODO code application logic here
+    }
+);
+}
+}
---
+++ src/javaapplication1/NewJFormDesignerForm2.java
@@ -0,0 +1,36 @@
+/*
+ * Created by JFormDesigner on Tue Oct 23 23:11:13 IST 2012
+ */
+
+package javaapplication1;
+
+import java.awt.*;
+import javax.swing.*;
+import com.jgoodies.forms.layout.*;
+
+/**
+ * @author unknown
+ */
+public class NewJFormDesignerForm2 extends JFrame {
+    public NewJFormDesignerForm2() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+        // Generated using JFormDesigner Evaluation license - unknown
+
+        //======== this ========
+        Container contentPane = getContentPane();
+        contentPane.setLayout(new FormLayout(
+            "default, $lcgap, default",
+            "2*(default, $lgap), default"));
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    // Generated using JFormDesigner Evaluation license - unknown
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
+}
